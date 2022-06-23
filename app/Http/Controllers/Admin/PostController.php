d7d5a018c7e2d984eb94a120dd9f4cb4aca23a36
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-06-23 17:17:26 +0200

    laravel: modifiche

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,10 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Post;
-
+use App\Category;
+use Prophecy\Call\Call;
 
 class PostController extends Controller
 {
+    protected $validationRule = [
+        "title" => "required|string|max:100",
+    ];
     /**
      * Display a listing of the resource.
      *
@@ -28,7 +32,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::all();
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
@@ -39,12 +44,14 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate($this->validationRule);
         $data = $request->all();
         $newPost = new Post();
         $newPost->title = $data['title'];
         $slug = Str::of($data['title'])->slug("-");
         $newPost->content = $data['content'];
         $newPost->published = isset($data['published']);
+        $newPost->category_id = $data['category_id'];
         $count = 1;
         while(Post::where('slug', $slug)->first()){
             $slug = Str::of($data['title'])->slug("-") . "-{$count}";
@@ -74,7 +81,10 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.posts.edit');
+        
+        $post = Post::findOrFail($id);
+        $categories = Category::all();
+        return view('admin.posts.edit', compact('post', 'categories'));
     }
 
     /**
@@ -84,9 +94,22 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $data = $request->all();
+        $request->validate($this->validationRule);
+        if($post->title != $data['title']){
+            $post->title = $data['title'];
+            $slug = Str::of($post->title)->slug('-');
+            if($slug != $post->slug){
+                $post->slug = $this->getSlug($post->title);
+        }
+    }
+        $post->category_id = $data['category_id'];
+        $post->content = $data['content'];
+        $post->published = isset($data['published']);
+        $post->update();
+        return redirect()->route('admin.posts.show', $post->id);
     }
 
     /**
@@ -95,8 +118,22 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        // $post->delete();
+        // return redirect()->route('admin.posts.index')->with('message', "Post with id: {$post->id} successfully deleted");
+    }
+    private function getSlug($title){
+
+        $slug = Str::of($title)->slug('-');
+        $count = 1;
+
+        while(Post::where('slug', $slug)->first()){
+            $slug = Str::of($title)->slug('-').'-{$count}';
+            $count++;
+        }
+
+        return $slug;
+
     }
 }
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -10,6 +10,9 @@
     </p>
     <small>{{$post->created_at}}</small>
     <h5>
-        Pubblicato: {{$post->published}}
+        Pubblicato: {{$post->published ? 'Published' : 'Unpublished'}}
     </h5>
+    <p>
+        <h2>{{$post->category ? $post->category->name : ''}}</h2>
+    </p>
 @endsection
